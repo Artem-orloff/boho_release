c2c18a0db46ffd816671380bccbe78452d2c95e6
artem <artem_orloff@mail.ru>
2025-11-02 19:36:57 +0600

    fixed window api

--- src/main/resources/static/config.js
+++ src/main/resources/static/config.js
@@ -3,5 +3,5 @@
   const isLocal = ['localhost', '127.0.0.1'].includes(location.hostname);
   window.BOHO_API_BASE = isLocal
     ? 'http://localhost:8080'
-    : 'https://api.artem-orloff-boho-release-a1ff.twc.net'; // или '' если фронт проксирует /api на тот же домен
+    : 'https://artem-orloff-boho-release-a1ff.twc.net'; // или '' если фронт проксирует /api на тот же домен
 })();
